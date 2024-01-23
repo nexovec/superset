4c5b5f43a0fdc5efae04bae425644a884e4ad18c
nexovec <hraccesky@gmail.com>
2024-01-23 13:09:32 +0100

    disabled talisman

--- superset/config.py
+++ superset/config.py
@@ -1421,7 +1421,7 @@ TEST_DATABASE_CONNECTION_TIMEOUT = timedelta(seconds=30)
 CONTENT_SECURITY_POLICY_WARNING = True
 
 # Do you want Talisman enabled?
-TALISMAN_ENABLED = utils.cast_to_boolean(os.environ.get("TALISMAN_ENABLED", True))
+TALISMAN_ENABLED = utils.cast_to_boolean(os.environ.get("TALISMAN_ENABLED", False))
 
 # If you want Talisman, how do you want it configured??
 TALISMAN_CONFIG = {
